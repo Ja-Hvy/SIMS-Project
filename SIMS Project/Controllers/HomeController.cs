c90089c3b9cd935ee90096e8136a886305c6e8ee
Ja-Hvy <nthhavy.05@gmail.com>
2025-11-11 00:19:41 -0600

    Add project files.

---
+++ SIMS Project/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
+using SIMS_Project.Models;
+using System.Diagnostics;
+
+namespace SIMS_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        // Thêm action Login 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
